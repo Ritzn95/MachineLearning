368472399da5242d46a68060bab443482530962b
Ritesh <ritzn95@gmail.com>
2024-10-17 20:32:51 +0530

    Plot functions

--- linear_regression_taxi.py
+++ linear_regression_taxi.py
@@ -91,3 +91,25 @@ print(answer)
 # -----------------------------------------------------------
 answer = '''The feature with the weakest correlation to the FARE is TIP_RATE.'''
 print(answer)
+
+# @title Define plotting functions
+
+
+def make_plots(df, feature_names, label_name, model_output, sample_size=200):
+
+    random_sample = df.sample(n=sample_size).copy()
+    random_sample.reset_index()
+    weights, bias, epochs, rmse = model_output
+
+    is_2d_plot = len(feature_names) == 1
+    model_plot_type = "scatter" if is_2d_plot else "surface"
+    fig = make_subplots(rows=1, cols=2,
+                        subplot_titles=("Loss Curve", "Model Plot"),
+                        specs=[[{"type": "scatter"}, {"type": model_plot_type}]])
+
+    plot_data(random_sample, feature_names, label_name, fig)
+    plot_model(random_sample, feature_names, weights, bias, fig)
+    plot_loss_curve(epochs, rmse, fig)
+
+    fig.show()
+    return
